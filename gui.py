d431a66aa1081d5dd9eff685d265c1f238f153c1
Brydzz <mrufkazkrem@protonmail.com>
2024-08-29 16:40:24 +0200

    created new label class, info for file and dir updates

--- gui.py
+++ gui.py
@@ -18,16 +18,6 @@ DRACULA_PALETTE = {
 }
 
 
-def get_filename():
-    filename = filedialog.askopenfilename()
-    print(filename)
-
-
-def get_directory():
-    directory = filedialog.askdirectory()
-    print(directory)
-
-
 # Main Window Config
 root = tk.Tk()
 root.title("Slides to Markdown")
@@ -52,17 +42,79 @@ info_frame = tk.Frame(
     highlightcolor=DRACULA_PALETTE["purple"],
     highlightthickness=2,
     bd=0,
+    padx=5,
+    pady=5,
 )
+
+
+# Labels Section
+class WrappingInfoLabel(tk.Label):
+    """a type of Label that automatically adjusts the wrap to the size
+    has a static text attribute that wont change when using update_text()"""
+
+    def __init__(self, master=None, static_text=None, **kwargs):
+        tk.Label.__init__(self, master, **kwargs, text=static_text)
+        self.stext = static_text
+        self.bind("<Configure>", self.update_wrap)
+
+    def update_wrap(self, event):
+        self.config(wraplength=self.master.winfo_width())
+
+    def update_text(self, new_text):
+        updated_text = f"{self.stext}\t{new_text}"
+        self.config(text=updated_text)
+
+
 label_font = tkFont.Font(family="Fira Code", size=10)
 
-info_label = tk.Label(
+info_label1 = WrappingInfoLabel(
     master=info_frame,
     font=label_font,
-    text="Selected File: N/A\nSelected Directory: N/A\nLogs:",
+    static_text="Selected File:",
     justify="left",
     fg=DRACULA_PALETTE["green"],
     background=DRACULA_PALETTE["background"],
+    anchor="w",
 )
+info_label2 = WrappingInfoLabel(
+    master=info_frame,
+    font=label_font,
+    static_text="Selected Directory:",
+    justify="left",
+    fg=DRACULA_PALETTE["green"],
+    background=DRACULA_PALETTE["background"],
+    anchor="w",
+)
+info_label3 = WrappingInfoLabel(
+    master=info_frame,
+    font=label_font,
+    static_text="Logs:",
+    justify="left",
+    fg=DRACULA_PALETTE["green"],
+    background=DRACULA_PALETTE["background"],
+    anchor="w",
+)
+
+
+# Label Functions
+def update_log_info(log):
+    old_text = info_label3["text"]
+    new_text = f"{old_text}\t{log}"
+    info_label3.config(text=new_text)
+
+
+# Button functions
+def get_filename():
+    filename = filedialog.askopenfilename()
+    info_label1.update_text(filename)
+    print(filename)
+
+
+def get_directory():
+    directory = filedialog.askdirectory()
+    info_label2.update_text(directory)
+    print(directory)
+
 
 # Font for buttons
 btn_font = tkFont.Font(family="Fira Code", size=12)
@@ -112,7 +164,10 @@ info_frame.grid(column=0, row=0, sticky="N S E W", columnspan=2, rowspan=2)
 filebtn.grid(column=0, row=2, columnspan=1, sticky="E W", padx=10)
 dirbtn.grid(column=1, row=2, columnspan=1, sticky="E W", padx=10)
 createbtn.grid(column=0, row=3, columnspan=2, sticky="E W", padx=10)
-info_label.grid(column=0, row=0)
+
+info_label1.grid(column=0, row=0, padx=5, sticky="E W")
+info_label2.grid(column=0, row=1, padx=5, sticky="E W")
+info_label3.grid(column=0, row=2, padx=5, sticky="E W")
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
